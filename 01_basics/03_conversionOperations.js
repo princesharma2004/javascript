fc46937e77fd76aa19a031f8a13b39589dc16e60
Prince Sharma <princesharma21102004@gmail.com>
2025-05-28 22:23:33 +0530

    operation on datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,5 +21,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let someString = String(someNumber)
-console.log(someString)
-console.log(typeof someString)
+// console.log(someString)
+// console.log(typeof someString)
+
+// ************************** operations **************************
+let value = 3
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "Hello"
+let str2 = " Prince"
+let str3 = str1 + str2
+// console.log(str3) // "Hello Prince"
+
+// console.log("2" + 2) // "22"
+// console.log(2 + "2") // "22"
+// console.log("1" + 2 + 2) // "122"
+// console.log(1 + 2 + "2") // "32"
