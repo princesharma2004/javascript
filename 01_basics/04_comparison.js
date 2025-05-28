c26bcb200041fcf76b4af9ea38aedbfe64318062
Prince Sharma <princesharma21102004@gmail.com>
2025-05-28 22:59:01 +0530

    Comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.lod(2 > 1) // true
+// console.log(2 < 1) // false
+// console.log(2 >= 1) // true
+// console.log(2 <= 1) // false
+// console.log(2 == 1) // false
+// console.log(2 != 1) // true
+
+// console.log("2" > 1) // true, string is converted to number
+// console.log("02" > 1) // true, string is converted to number
+
+// console.log(null > 0) // false, null is converted to 0
+// console.log(null >= 0) // true, null is converted to 0
+// console.log(null < 0) // false, null is converted to 0
+// console.log(null <= 0) // true, null is converted to 0
+
+// console.log(undefined > 0) // false, undefined is not a number
+// console.log(undefined >= 0) // false, undefined is not a number
+// console.log(undefined < 0) // false, undefined is not a number
+// console.log(undefined <= 0) // false, undefined is not a number
+
+// ===
+
+// console.log("2" === 2) // false, strict equality, no type conversion
