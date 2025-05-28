433b5801d10c91c08f73923de9da5fd75f654917
Prince Sharma <princesharma21102004@gmail.com>
2025-05-28 22:08:58 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let scoreNumber = Number(score)
+// console.log(typeof scoreNumber)
+// console.log(scoreNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "abc" => true; "" => false
+
+let someNumber = 33
+let someString = String(someNumber)
+console.log(someString)
+console.log(typeof someString)
